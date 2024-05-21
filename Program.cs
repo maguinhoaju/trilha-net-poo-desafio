05548d50940d7fd5be341445c69628bccce780f6
Carlos Magno de França Veiga <32390057+maguinhoaju@users.noreply.github.com>
2024-05-21 08:02:15 -0300

    Classe main de testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia1 = new Nokia("79988525233", "C20", "ABCD1234", 6);
+Nokia nokia2 = new Nokia("79999999999", "C30", "ABCD1111", 4);
+Iphone iPhone1 = new Iphone("79111111111", "15 Pro Max", "XPTO1111", 4);
+Iphone iPhone2 = new Iphone("79222222222", "13 Pro", "QWERTY1111", 3);
+
+nokia1.InstalarAplicativo("WhatsApp");
+nokia2.InstalarAplicativo("Instagram");
+iPhone1.InstalarAplicativo("WhatsApp");
+iPhone2.InstalarAplicativo("Facebook");
+nokia1.Ligar();
+iPhone1.Ligar();
+nokia2.ReceberLigacao();
+iPhone2.ReceberLigacao();
